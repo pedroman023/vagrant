81fb83f696e96dae183c4ec58b0697de2797fd83
Pedro Man <pedro.man023@gmail.com>
2021-08-09 08:40:30 +0100

    first push, 3 VMs configured for kubernetes environment

---
+++ bootstrap.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo "Starting bootstrap.sh!"
+sudo apt-get -y update && sudo apt-get -y upgrade && sudo apt-get -y dist-upgrade
+sudo apt-get install -y apt-transport-https ca-certificates curl
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
+sudo apt -y autoremove
+echo "Finished bootstrap.sh!"
