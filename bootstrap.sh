da092a86f9c03e5cb89e27c234a2ac3140ac8ed1
Pedro Man <pedro.man023@gmail.com>
2021-08-10 19:12:24 +0100

    added docker install to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,6 @@ sudo apt-get install -y apt-transport-https ca-certificates curl
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
-sudo apt-get install -y kubectl
+sudo apt-get install -y kubectl docker.io
 sudo apt -y autoremove
 echo "Finished bootstrap.sh!"
